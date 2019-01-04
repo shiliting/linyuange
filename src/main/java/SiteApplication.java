7b4c577f41e6fc6bc86edd15bce665096246912a
shiliting <861819089@qq.com>
2019-01-04 22:58:20 +0800

    hello启动

--- src/main/java/SiteApplication.java
+++ src/main/java/SiteApplication.java
@@ -1,8 +1,16 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableAutoConfiguration
+@RestController
 public class SiteApplication {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
     public static void main(String[] args){
         SpringApplication.run(SiteApplication.class, args);
     }
