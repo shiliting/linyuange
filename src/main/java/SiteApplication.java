02b48d050cffeae341e3b34f228d56335c7f1cce
shiliting <861819089@qq.com>
2019-01-04 22:53:09 +0800

    提交启动文件

---
+++ src/main/java/SiteApplication.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
+public class SiteApplication {
+    public static void main(String[] args){
+        SpringApplication.run(SiteApplication.class, args);
+    }
+}
